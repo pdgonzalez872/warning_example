df87e7158abb51bf22e833ceaf5183a9292ce666
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2019-11-20 08:05:00 -0600

    mix new

---
+++ lib/warning.ex
@@ -0,0 +1,18 @@
+defmodule Warning do
+  @moduledoc """
+  Documentation for Warning.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Warning.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
